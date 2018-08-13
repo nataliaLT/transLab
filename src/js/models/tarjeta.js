a4a8ffb261b4a9d971cacedc7f31101e113fabc7
natalia <natalia.lucerot@gmail.com>
2018-08-13 15:17:38 -0500

    Diseño de paginas de pregunta

---
+++ src/js/models/tarjeta.js
@@ -0,0 +1,8 @@
+firebase.initializeApp({
+    apiKey: 'AIzaSyA1x5slD71Pr0ASFydAeIMYlm-DCVPMVZI',
+    authDomain: 'translab-686ee.firebaseapp.com',
+    projectId: 'translab-686ee'
+  });
+  
+  // Initialize Cloud Firestore through Firebase
+  var db = firebase.firestore();
