c3bc6a5d7e3dc86ca4ee9322a92d86420f7841c0
natalia <natalia.lucerot@gmail.com>
2018-10-20 14:51:01 -0500

    se guarda los datos del registro

--- src/js/models/main.js
+++ src/js/models/main.js
@@ -1,20 +1,17 @@
-let formulario = document.getElementById('formulario');
-//funcion para guardar usuarios 
+
 function registrar(){
-    const correo= document.getElementById('correo').value;
-    const contraseña= document.getElementById('contraseña').value;
-    firebase.auth().createUserWithEmailAndPassword(correo, contraseña)
-    .the((Response)=>{
-        location.href= "home.html";
-    })
-    .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode);
-        console.log(errorMessage);
-        // ...
-      });
+let correo = document.getElementById('correo').value;
+let contraseña = document.getElementById('contraseña').value;
+
+firebase.auth().createUserWithEmailAndPassword(correo, contraseña)
+.catch(function(error) {
+    
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+  });
+  
+
 }
-formulario.addEventListener('submit', registrar)
 
