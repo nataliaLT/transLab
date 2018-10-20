3e5aeff32d150387aea5c737360503c0d739d501
natalia <natalia.lucerot@gmail.com>
2018-10-20 16:52:09 -0500

    registro y login funcionan

--- src/js/models/main.js
+++ src/js/models/main.js
@@ -1,16 +1,28 @@
 //función registro de usuarios
-function registrar(){
-let correo = document.getElementById('correo').value;
-let contraseña = document.getElementById('contraseña').value;
-
-firebase.auth().createUserWithEmailAndPassword(correo, contraseña)
-.catch(function(error) {
+let formulario = document.getElementById('formulario').value;
+formulario.addEventListener('submit',e =>{
+    let correo = document.getElementById('correo').value;
+    let contraseña = document.getElementById('contraseña').value;
     
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorCode);
-    console.log(errorMessage);
-  });
+    firebase.auth().createUserWithEmailAndPassword(correo, contraseña)
+    .then((Response =>{
+        const userId = Response.user.Id;
+        firebase.database().ref('users/' + userId).set({
+            correo : correo,
+            contraseña: contraseña
+        })
+        console.log('ya envie los datos');
+    }).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+      })
+    )}
+)
+
+function registrar(){
+    location.href="home.html";
 }
 
 
