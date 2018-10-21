09496d2ebd367b961bf86e68d2f95dbaf5624547
natalia <natalia.lucerot@gmail.com>
2018-10-21 15:29:50 -0500

    calcular saldo

--- src/js/models/calcular.js
+++ src/js/models/calcular.js
@@ -1,31 +1,61 @@
-
-const saldo = document.getElementById('saldo');
-//Funcion para llamar a la api
-saldo.addEventListener('click',()=>{
-    
-    let tarjeta=document.getElementById("nTarjeta").value;//variable que guardara el numero de tarjeta ingresado
+function calcularSaldo(){
+    const tarjeta = document.getElementById('numTar').value;
+    document.getElementById('numTar').value = '';
     fetch(`http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=${tarjeta}`)//llamamos a la api
     .then(response => response.json())
     .then(data=>{
-        const dataBip = Object.values(data)
-        let saldoBip = dataBip[2];
+                const datoBip = Object.values(data)
+                let saldo = datoBip[2];
+        
+                let saldoBipRep = Number(saldo.replace(/[$,.]+/g, ""));
+                console.log(saldoBipRep)
+        
+                const combo = document.getElementById("seleccionHorario");
+                const horarios = combo.options[combo.selectedIndex].value;
+        
+                const saldoFinal = saldoBipRep - horarios;
+        
+        
+                document.getElementById("costoPasaje").innerHTML =
+                    `<span class='col d-flex justify-content-center p-0 mt-4' id='txtCosto'>COSTO PASAJE</span>
+                <span class='col d-flex justify-content-center p-0' id='costoPasaje'>${horarios}</span>`;
+                document.getElementById("saldoFinal").innerHTML =
+                    `<span class='col d-flex justify-content-center p-0 mt-3' id='txtCosto'>SALDO FINAL</span>
+                <span class='col d-flex justify-content-center p-0' id='costoPasaje'>${saldoFinal}</span>`;
+                console.log(data);
+                renderInfo(data);
+        })
+    }
 
-        let saldoBipRep = Number(saldoBip.replace(/[$,.]+/g, ""));
-
-        const combo = document.getElementById("selectHorarios");
-        const horarios = combo.options[combo.selectedIndex].value;
-
-        const saldoFinal = saldoBipRep - horarios;
 
 
-        document.getElementById("costoPasaje").innerHTML =
-            `<span class='col d-flex justify-content-center p-0 mt-4' id='txtCosto'>COSTO PASAJE</span>
-        <span class='col d-flex justify-content-center p-0' id='costoPasaje'>${horarios}</span>`;
-        document.getElementById("saldoFinal").innerHTML =
-            `<span class='col d-flex justify-content-center p-0 mt-3' id='txtCosto'>SALDO FINAL</span>
-        <span class='col d-flex justify-content-center p-0' id='costoPasaje'>${saldoFinal}</span>`;
-        console.log(data);
-        renderInfo(data);
-})
-});
+// const saldo = document.getElementById('saldo');
+// //Funcion para llamar a la api
+// saldo.addEventListener('click',()=>{
+    
+//     let tarjeta=document.getElementById("nTarjeta").value;//variable que guardara el numero de tarjeta ingresado
+//     fetch(`http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=${tarjeta}`)//llamamos a la api
+//     .then(response => response.json())
+//     .then(data=>{
+//         const dataBip = Object.values(data)
+//         let saldoBip = dataBip[2];
+
+//         let saldoBipRep = Number(saldoBip.replace(/[$,.]+/g, ""));
+
+//         const combo = document.getElementById("selectHorarios");
+//         const horarios = combo.options[combo.selectedIndex].value;
+
+//         const saldoFinal = saldoBipRep - horarios;
+
+
+//         document.getElementById("costoPasaje").innerHTML =
+//             `<span class='col d-flex justify-content-center p-0 mt-4' id='txtCosto'>COSTO PASAJE</span>
+//         <span class='col d-flex justify-content-center p-0' id='costoPasaje'>${horarios}</span>`;
+//         document.getElementById("saldoFinal").innerHTML =
+//             `<span class='col d-flex justify-content-center p-0 mt-3' id='txtCosto'>SALDO FINAL</span>
+//         <span class='col d-flex justify-content-center p-0' id='costoPasaje'>${saldoFinal}</span>`;
+//         console.log(data);
+//         renderInfo(data);
+// })
+// });
 
